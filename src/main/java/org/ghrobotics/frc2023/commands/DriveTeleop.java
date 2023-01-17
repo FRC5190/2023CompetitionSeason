a7a3ad71d2cadcc17f33afaebf3419f51d2a228f
diya226 <50601301+diya226@users.noreply.github.com>
2023-01-17 12:22:34 -0500

    Basic SparkMAX Drivetrain Code & Test PathPlanner

--- src/main/java/org/ghrobotics/frc2023/commands/DriveTeleop.java
+++ src/main/java/org/ghrobotics/frc2023/commands/DriveTeleop.java
@@ -4,6 +4,32 @@
 
 package org.ghrobotics.frc2023.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import org.ghrobotics.frc2023.subsystems.Drivetrain;
+
 
 /** Add your docs here. */
-public class DriveTeleop {}
+public class DriveTeleop extends CommandBase{
+    private final Drivetrain drivetrain_;
+    private final XboxController controller_;
+
+    public DriveTeleop(Drivetrain drivetrain, XboxController controller) {
+        drivetrain_ = drivetrain;
+        controller_ = controller;
+    }
+
+    @Override
+    public void execute(){
+        double forward = -controller_.getLeftY();
+        double curvature = controller_.getLeftX();
+        boolean quick_turn = controller_.getXButton();
+
+        DifferentialDrive.WheelSpeeds speeds = DifferentialDrive.curvatureDriveIK(
+            forward, curvature, quick_turn);
+
+        drivetrain_.setPercent(speeds.left, speeds.right);
+    }
+
+}
--- src/main/java/org/ghrobotics/frc2023/subsystems/Drivetrain.java
+++ src/main/java/org/ghrobotics/frc2023/subsystems/Drivetrain.java
@@ -4,23 +4,82 @@
 
 package org.ghrobotics.frc2023.subsystems;
 
+import edu.wpi.first.math.MathUtil;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 /** Add your docs here. */
 public class Drivetrain extends SubsystemBase{
     //Motor Controllers
-    /*private final CANSparkMax left_leader_;
+    private final CANSparkMax left_leader_;
     private final CANSparkMax left_follower_;
     private final CANSparkMax right_leader_;
-    private final CANSparkMax right_follower_;*/
+    private final CANSparkMax right_follower_;
+
+    //Output Limit
+    private boolean limit_output = false;
+
+    //IO
+    private OutputType output_type_ = OutputType.PERCENT;
+    private final PeriodicIO io_ = new PeriodicIO();
 
     public Drivetrain(){
-        /*left_leader_ = new CANSparkMax(0);
-        left_follower_ = new CANSparkMax(1);
-        right_leader_ = new CANSparkMax(2);
-        right_follower_ = new CANSparkMax(3);*/
+        //Initialize motor controllers
+        left_leader_ = new CANSparkMax(Constants.kLeftLeaderId, MotorType.kBrushless);
+        left_leader_.setInverted(false);
 
+        left_follower_ = new CANSparkMax(Constants.kLeftFollowerId, MotorType.kBrushless);
+        left_follower_.follow(left_leader_);
+
+        right_leader_ = new CANSparkMax(Constants.kRightLeaderId, MotorType.kBrushless);
+        right_leader_.setInverted(true);
+
+        right_follower_ = new CANSparkMax(Constants.kRightFollowerId, MotorType.kBrushless);
+        right_follower_.follow(right_follower_);
+
+    }
+
+    @Override
+    public void periodic(){
+        switch (output_type_){
+            case PERCENT:
+                left_leader_.set(limit_output ? 
+                    MathUtil.clamp(io_.l_demand, -Constants.kOutputLimit, Constants.kOutputLimit) : 
+                    io_.l_demand);
+                right_leader_.set(limit_output ? 
+                    MathUtil.clamp(io_.r_demand, -Constants.kOutputLimit, Constants.kOutputLimit) : 
+                    io_.r_demand);
+        }
+    }
+
+    public void setPercent(double l, double r) {
+        output_type_ = OutputType.PERCENT;
+        io_.l_demand = l;
+        io_.r_demand = r;
+    }
+
+    enum OutputType {
+        PERCENT
+      }
+
+    public static class PeriodicIO {
+        //Inputs
+
+        //Outputs
+        double l_demand;
+        double r_demand;
+    }
+
+    public static class Constants {
+        //Motor Controller IDs
+        public static final int kLeftLeaderId = 1; 
+        public static final int kLeftFollowerId = 2;
+        public static final int kRightLeaderId = 3;
+        public static final int kRightFollowerId = 4;
+
+        //Output Limit
+        public static final double kOutputLimit = 0.3;
     }
 }
