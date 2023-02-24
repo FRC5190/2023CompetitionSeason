a21f23f485d679db60ca2b155de5a0d027777c80
diya226 <50601301+diya226@users.noreply.github.com>
2023-02-23 23:27:26 -0500

    add april tag blue origin positions

--- src/main/java/org/ghrobotics/frc2023/Arena.java
+++ src/main/java/org/ghrobotics/frc2023/Arena.java
@@ -13,22 +13,22 @@ import edu.wpi.first.math.geometry.Translation3d;
 public class Arena {
 
     public static final Pose3d[] tagPositions = new Pose3d[]{
-        new Pose3d(0, 0, 0, null), //1
-        new Pose3d(0, 0, 0, null), //2
-        new Pose3d(0, 0, 0, null), //3
-        new Pose3d(0, 0, 0, null), //6
-        new Pose3d(0, 0, 0, null), //7
-        new Pose3d(0, 0, 0, null), //8
+        new Pose3d(15.513, 1.0734, 0.46272, null), //1
+        new Pose3d(15.513, 2.7498, 0.46272, null), //2
+        new Pose3d(15.513, 4.4262, 0.46272, null), //3
+        new Pose3d(1.0269, 4.4262, 0.46272, null), //6
+        new Pose3d(1.0269, 2.7498, 0.46272, null), //7
+        new Pose3d(1.0269, 1.0734, 0.46272, null), //8
     };
 
    public static final Transform3d[] blueTransform = {
-                    new Transform3d(new Translation3d(0.6096, -0.7366, -0.4953), new Rotation3d()), //Left
+                    new Transform3d(new Translation3d(0.6096, -0.7366, -0.46272), new Rotation3d()), //Left
                     new Transform3d(new Translation3d(0, 0, 0), new Rotation3d()), //Center
                     new Transform3d(new Translation3d(0, 0, 0), new Rotation3d()),//Right
                     }; 
 
     public static final Transform3d[] redTransform = {
-        new Transform3d(new Translation3d(-0.6096, 0.7366, -0.4953), new Rotation3d()), //Left
+        new Transform3d(new Translation3d(-0.6096, 0.7366, -0.46272), new Rotation3d()), //Left
         new Transform3d(new Translation3d(0, 0, 0), new Rotation3d()), //Center
         new Transform3d(new Translation3d(0, 0, 0), new Rotation3d()),//Right
         }; 
