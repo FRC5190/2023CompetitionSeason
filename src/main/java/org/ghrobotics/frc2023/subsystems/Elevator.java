a51563ab4c6ce7e406005a1033c5207429cd2d93
amy-acharya <amy.acharya.01@gmail.com>
2023-03-02 19:53:19 -0500

    Add extender

--- src/main/java/org/ghrobotics/frc2023/subsystems/Elevator.java
+++ src/main/java/org/ghrobotics/frc2023/subsystems/Elevator.java
@@ -8,7 +8,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
--- src/main/java/org/ghrobotics/frc2023/subsystems/Extender.java
+++ src/main/java/org/ghrobotics/frc2023/subsystems/Extender.java
@@ -4,21 +4,125 @@
 
 package org.ghrobotics.frc2023.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless;
 
 public class Extender extends SubsystemBase {
-  /**
-   * Creates a new Extender.
-   */
+  // Motor Controllers
+  private final CANSparkMax leader_;
+
+  // Encoder
+  private final RelativeEncoder encoder_;
+
+  // Control
+  private final ProfiledPIDController fb_;
+  private final SimpleMotorFeedforward ff_;
+
+  // IO
+  private final PeriodicIO io_ = new PeriodicIO();
+  private OutputType output_type_ = OutputType.PERCENT;
+
+
   public Extender() {
+    // Initialize motor controllers
+    leader_ = new CANSparkMax(Constants.kLeaderId, kBrushless);
 
-    //1 motor
-    //1 gearbox
 
+    // Initialize encoder
+    encoder_ = leader_.getEncoder();
+    encoder_.setPositionConversionFactor(
+        Math.PI * Constants.kSprocketDiameter / Constants.kGearRatio);
+    encoder_.setVelocityConversionFactor(
+        Math.PI * Constants.kSprocketDiameter / Constants.kGearRatio / 60);
+
+    // Initialize control
+    ff_ = new SimpleMotorFeedforward(Constants.kS, Constants.kV, Constants.kA);
+    fb_ = new ProfiledPIDController(Constants.kP, 0, 0, new TrapezoidProfile.Constraints(
+        Constants.kMaxVelocity, Constants.kMaxAcceleration));
+
+    // Safety features
+    leader_.setSmartCurrentLimit((int) Constants.kCurrentLimit);
+    leader_.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, (float) Constants.kMinLength);
+    leader_.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, (float) Constants.kMaxLength);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // Read inputs
+    io_.position = encoder_.getPosition();
+    io_.velocity = encoder_.getVelocity();
+
+    // Write outputs
+    switch (output_type_) {
+      case PERCENT:
+        leader_.set(io_.demand);
+        break;
+      case POSITION:
+        fb_.setGoal(io_.demand);
+        double feedback = fb_.calculate(io_.position);
+
+        double velocity_setpoint = fb_.getSetpoint().velocity;
+        double acceleration_setpoint = (velocity_setpoint - io_.velocity) / 0.02;
+        double feedforward = ff_.calculate(velocity_setpoint, acceleration_setpoint);
+
+        leader_.setVoltage(feedback + feedforward);
+        break;
+    }
+  }
+
+  public void setPercent(double percent) {
+    output_type_ = OutputType.PERCENT;
+    io_.demand = percent;
+  }
+
+  public void setPosition(double position) {
+    output_type_ = OutputType.POSITION;
+    io_.demand = position;
+  }
+
+  // Output Type
+  private enum OutputType {
+    PERCENT, POSITION
+  }
+
+  // IO
+  private static class PeriodicIO {
+    // Inputs
+    double position;
+    double velocity;
+
+    // Outputs
+    double demand;
+  }
+
+  // Constants
+  private static class Constants {
+    // Motor Controllers
+    public static final int kLeaderId = 7;
+
+    // Physical Constants
+    public static final double kGearRatio = 12.0;
+    public static final double kSprocketDiameter = 0.045;
+    public static final double kMinLength = 0.0;
+    public static final double kMaxLength = Units.inchesToMeters(40);
+
+    // Feedforward
+    public static final double kS = 0.0;
+    public static final double kV = 0.0;
+    public static final double kA = 0.0;
+
+    // Current Limit
+    public static final double kCurrentLimit = 50;
+
+    // Control
+    public static double kMaxVelocity = 0.3;
+    public static double kMaxAcceleration = 0.3;
+    public static double kP = 0.0;
   }
 }
