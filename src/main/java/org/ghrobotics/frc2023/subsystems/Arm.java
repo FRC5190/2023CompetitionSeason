64b29c90695b615724cc632409651fd7538ba86a
diya226 <50601301+diya226@users.noreply.github.com>
2023-03-03 20:37:43 -0500

    Create Arm Subsystem

--- src/main/java/org/ghrobotics/frc2023/subsystems/Arm.java
+++ src/main/java/org/ghrobotics/frc2023/subsystems/Arm.java
@@ -4,21 +4,127 @@
 
 package org.ghrobotics.frc2023.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
-  /**
-   * Creates a new Arm.
-   */
+  //Motor Controller
+  private final CANSparkMax leader_;
+
+  //Encoder
+  private final RelativeEncoder encoder_;
+
+  //Control
+  private final ArmFeedforward ff_;
+  private final ProfiledPIDController fb_;
+
+  //IO
+  private final PeriodicIO io_ = new PeriodicIO();
+  private OutputType output_type_ = OutputType.PERCENT;
+
+
+  //Constructor
   public Arm() {
+    //Initialize motor controllers
+    leader_ = new CANSparkMax(Constants.kLeaderId, MotorType.kBrushless);
+    leader_.restoreFactoryDefaults();
 
-    //1 neo
-    //1 gearbox
+    //Initialize encoder
+    encoder_ = leader_.getEncoder();
+    encoder_.setPositionConversionFactor(((Constants.kSmallSprocketDiameter / Constants.kBigSprocketDiameter) / Constants.kGearRatio) * 2 * Math.PI);
+    encoder_.setVelocityConversionFactor(((Constants.kSmallSprocketDiameter / Constants.kBigSprocketDiameter) / Constants.kGearRatio) * (2 * Math.PI) / 60);
 
+    //Initialize control
+    ff_ = new ArmFeedforward(Constants.kS, Constants.kG, Constants.kV, Constants.kA);
+    fb_ = new ProfiledPIDController(Constants.kP, 0, 0, new TrapezoidProfile.Constraints(
+      Constants.kMaxVelocity, Constants.kMaxAcceleration));
+
+    //Safety features
+    leader_.setSmartCurrentLimit((int) Constants.kCurrentLimit);
+    leader_.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, (float) Constants.kMinAngle);
+    leader_.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, (float) Constants.kMaxAngle);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    //Read inputs
+    io_.angle = encoder_.getPosition();
+    io_.angular_velocity = encoder_.getVelocity();
+
+    //Write outputs
+    switch (output_type_){
+      case PERCENT:
+        leader_.set(io_.demand);
+        break;
+      case ANGLE:
+        fb_.setGoal(io_.demand);
+        double feedback = fb_.calculate(io_.angle);
+
+        double velocity_setpoint = fb_.getSetpoint().velocity;
+        double acceleration_setpoint = (velocity_setpoint - io_.angular_velocity) / 0.02;
+        double feedforward = ff_.calculate(velocity_setpoint, acceleration_setpoint);
+
+        leader_.setVoltage(feedback + feedforward);
+        break;
+    }
+  }
+
+  public void setPercent(double percent){
+    output_type_ = OutputType.PERCENT;
+    io_.demand = percent;
+  }
+
+  public void setAngle(double angle) {
+    output_type_ = OutputType.ANGLE;
+    io_.demand = angle;
+  }
+
+  //Output Type
+  private enum OutputType {
+    PERCENT, ANGLE
+  }
+
+  //IO
+  private static class PeriodicIO {
+    //Inputs
+    double angle;
+    double angular_velocity;
+
+    //Outputs
+    double demand;
+  }
+
+  //Constants
+  private static class Constants {
+    //Motor Controller
+    public static final int kLeaderId = 8;
+
+    //Physical Constants
+    public static final double kGearRatio = 49.0;
+    public static final double kSmallSprocketDiameter = Units.inchesToMeters(1.790);
+    public static final double kBigSprocketDiameter = Units.inchesToMeters(4.303);
+    public static final double kMinAngle = 0.0;
+    public static final double kMaxAngle = 0.0;
+
+    //Feedforward
+    public static final double kA = 0.0; //volts * seconds^2 / radians
+    public static final double kG = 0.0; //volts
+    public static final double kS = 0.0; //volts
+    public static final double kV = 0.0; //volts * seconds/radians
+
+    //Current Limit
+    public static final double kCurrentLimit = 50;
+
+    //Control
+    public static double kMaxVelocity = 0.3;
+    public static double kMaxAcceleration = 0.3;
+    public static double kP = 0.0;
   }
 }
